6c890c39f47221872610476f250bd363f1683e39
Kaung Myat Han <arkarhtethan@gmail.com>
2022-11-12 15:51:32 +0630

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,21 @@
+import Logger from "bunyan";
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const log: Logger = config.createLogger("setupDatabase")
+
+export default () => {
+    const connect = () => {
+
+        mongoose.connect(config.DATABASE_URL!)
+            .then(() => {
+                log.info("Successfully connected to the databse.")
+            })
+            .catch(err => {
+                log.error("Erro connecting to databse", err)
+                return process.exit(1)
+            })
+    }
+    connect();
+    mongoose.connection.on("disconnected", connect)
+}
